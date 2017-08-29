7976893c121b42ace425665eec463cc6a765e1ea
caiyun792477396 <792477396@qq.com>
2017-08-30 04:15:50 +0800

    django-mysite

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from .models import Question,Choice
 from django.template import loader
 def index(request):
@@ -27,8 +28,13 @@ def detail(request,question_id):
 #def results(request,question_id):
 #    response = "You're looking at the results of question %s."
 #    return HttpResponse(response % question_id)
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
+
 def vote(request,question_id):
 #    return HttpResponse("You're voting on question %s." % question_id)
+#    import pdb;pdb.set_trace()
     question=get_object_or_404(Question,pk=question_id)
     try:
         selected_choice=question.choice_set.get(pk=request.POST['choice'])
@@ -39,7 +45,4 @@ def vote(request,question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
 #        return HttpResponse(response % selected_choice.votes)
-def results(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
 # Create your views here.
